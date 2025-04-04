371cef62a3dbb6ba2ccbad512832bca0b565dfde
kongs <vividsunmi@snu.ac.kr>
2025-04-04 12:39:33 +0900

    feat: 상태관리 Provider 구현

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import '../models/product.dart';
+import '../models/cart_item.dart';
+
+class CartProvider with ChangeNotifier {
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+
+  void addToCart(Product product, int quantity) {
+    final index = _items.indexWhere((item) => item.product.id == product.id);
+    if (index >= 0) {
+      _items[index].quantity += quantity;
+    } else {
+      _items.add(CartItem(product: product, quantity: quantity));
+    }
+    notifyListeners();
+  }
+
+  void updateQuantity(Product product, int quantity) {
+    final index = _items.indexWhere((item) => item.product.id == product.id);
+    if (index >= 0) {
+      _items[index].quantity = quantity;
+      notifyListeners();
+    }
+  }
+
+  void remove(Product product) {
+    _items.removeWhere((item) => item.product.id == product.id);
+    notifyListeners();
+  }
+
+  void clearCart() {
+    _items.clear();
+    notifyListeners();
+  }
+
+  int get totalPrice =>
+      _items.fold(0, (sum, item) => sum + item.product.price * item.quantity);
+
+  bool isInCart(Product product) {
+    return _items.any((item) => item.product.id == product.id);
+  }
+}
